36ba9e6550f56f99d98494759cdc65de54f564ee
Nikita Popov <Nikita.Popov@itechart-group.com>
2021-12-14 16:35:03 +0300

    Created Home and Starred pages

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom';
+
+import Navs from './components/Navs';
+import Home from './pages/Home';
+import Starred from './pages/Starred';
 
 function App() {
   return (
-    <Switch>
-      <Route exact path='/'>
-        This is Home Page
-      </Route>
-      <Route exact path='/starred'>
-        This is Starred
-      </Route>
-      <Route>
-        This is 404 Page
-      </Route>
-    </Switch>
-  );
+    <div>
+      <Navs />
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route exact path='/starred'>
+          <Starred />
+        </Route>
+        <Route>
+          <div>
+            Sorry, the page is not found
+          </div>
+        </Route>
+      </Switch>
+    </div>
+  )
 }
 
 export default App;
---
+++ src/pages/Starred.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Starred() {
+  return (
+    <div>
+      This is Starred
+    </div>
+  )
+}
+
+export default Starred;
