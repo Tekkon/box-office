da7e981065987fdc5e6389799cfbb875d646d3c7
Nikita Popov <Nikita.Popov@itechart-group.com>
2022-09-04 18:06:04 +0500

    Styled navs and title

--- src/App.js
+++ src/App.js
@@ -4,28 +4,39 @@ import { Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Starred from './pages/Starred';
 import Show from './pages/Show';
+import { ThemeProvider } from 'styled-components';
+
+const theme = {
+  mainColors: {
+    blue: '#2400ff',
+    gray: '#c6c6c6',
+    dark: '#353535',
+  },
+};
 
 function App() {
   return (
-    <Switch>
-      <Route exact path='/'>
-        <Home />
-      </Route>
+    <ThemeProvider theme={theme}>
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
 
-      <Route exact path='/starred'>
-        <Starred />
-      </Route>
+        <Route exact path='/starred'>
+          <Starred />
+        </Route>
 
-      <Route exact path='/shows/:id'>
-        <Show />
-      </Route>
+        <Route exact path='/shows/:id'>
+          <Show />
+        </Route>
 
-      <Route>
-        <div>
-          Sorry, the page is not found
-        </div>
-      </Route>
-    </Switch>
+        <Route>
+          <div>
+            Sorry, the page is not found
+          </div>
+        </Route>
+      </Switch>
+    </ThemeProvider>
   )
 }
 
