c6346848c6c5925f25e2f9113dc40d996f1cbd4a
Nikita Popov <Nikita.Popov@itechart-group.com>
2022-09-06 14:58:35 +0500

    Updates

---
+++ precache-manifest.e9b87ed878a7853eea1dca9da7ce64db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba0eaa4cb5eff5fc518ea6238b2e5ee0",
+    "url": "/box-office/index.html"
+  },
+  {
+    "revision": "3ed43b7f7923878a1a7e",
+    "url": "/box-office/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7f680fec73b569e661c7",
+    "url": "/box-office/static/js/2.f0458329.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/box-office/static/js/2.f0458329.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ed43b7f7923878a1a7e",
+    "url": "/box-office/static/js/main.ebf5fdc5.chunk.js"
+  },
+  {
+    "revision": "a24731daa79adae5d2cf",
+    "url": "/box-office/static/js/runtime-main.796231bd.js"
+  }
+]);
